67d03619c0b85629e48de4732c97de520bd9c141
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2022-01-04 18:40:29 +0000

    committing with water bottle getVolume function

--- WaterBottle/src/test/java/WaterBottleTest.java
+++ WaterBottle/src/test/java/WaterBottleTest.java
@@ -1,2 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+    WaterBottle myWaterBottle;
+
+    @Before
+    public void before(){
+        myWaterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100,myWaterBottle.getVolume());
+    }
 }
